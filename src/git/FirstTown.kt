dc42ff01304ac025e971e0ead23332ee5121481f
JuneDeveloper <ivan.iw.ko2017@gmail.com>
2024-12-25 19:34:55 +0800

    4.Создан класс первого города, FirstTown, он наследует интерфейс и абстрактный класс.

---
+++ src/git/FirstTown.kt
@@ -0,0 +1,18 @@
+package git
+
+class FirstTown(name: String): Town(name),InfoSity {
+
+
+
+    override fun getModelInfo(): List<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getChoise() {
+        TODO("Not yet implemented")
+    }
+
+    override fun printObjectInfo() {
+        TODO("Not yet implemented")
+    }
+}
